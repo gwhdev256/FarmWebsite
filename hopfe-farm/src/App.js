9e27feaefdcac28dc65448fc6ca49551ed966e92
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-03-29 10:50:08 -0600

    tweaked css values for icons, added tooltips

--- hopfe-farm/src/App.js
+++ hopfe-farm/src/App.js
@@ -43,7 +43,22 @@ const App = () => {
           Hopfe Farm
         </div>
         <div className="Nav-bar">
-          {navIcons()}
+          <div className="Home-nav">
+            {<img key={1} name={home} src={home} tabIndex={0} className={(selectedIcon === home) ? `Selected home` : `Unselected home`} onClick={(event) => selectedElement(event)} />}
+            <span className="Tooltip home-tip">Home Page</span>
+          </div>
+          <div className="Hay-nav">
+            <img key={2} name={hay} src={hay} tabIndex={0} className={(selectedIcon === hay) ? `Selected hay` : `Unselected hay`} onClick={(event) => selectedElement(event)} />
+            <span className="Tooltip hay-tip">Hay</span>
+          </div>
+          <div className="Honey-nav">
+            <img key={3} name={honey} src={honey} tabIndex={0} className={(selectedIcon === honey) ? `Selected honey` : `Unselected honey`} onClick={(event) => selectedElement(event)} />
+            <span className="Tooltip honey-tip">Honey</span>
+          </div>
+          <div className="Contact-nav">
+            <img key={4} name={contact} src={contact} tabIndex={0} className={(selectedIcon === contact) ? `Selected contact` : `Unselected contact`} onClick={(event) => selectedElement(event)} />
+            <span className="Tooltip contact-tip">Contact Us</span>
+          </div>
         </div>
       </header>
       <div className="Component-display">
