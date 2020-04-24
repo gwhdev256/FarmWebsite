f33a57808a0e807ed78f9dc76250a928ab919666
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-04-24 10:12:11 -0600

    built honey table

--- hopfe-farm/src/components/AppContext.js
+++ hopfe-farm/src/components/AppContext.js
@@ -7,10 +7,14 @@ export const ContextProvider = () => {
     const [hayHeader, setHayHeader] = useState(["Hay Type", "Bale Quality", "Quantity", "Price/Bale"]);
     const [hayTr1, setHayTr1] = useState(["5% Alfalfa", "No Rain", (5).toFixed(0), "$ "+(125.00).toFixed(2)]);
     const [hayTr2, setHayTr2] = useState(["30% Alfalfa", "No Rain", (39).toFixed(0), "$ "+(120.00).toFixed(2)]);
-    const [hayTr3, setHayTr3] = useState(["5% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(110.00).toFixed(2)]);
-    const [hayTr4, setHayTr4] = useState(["30% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(105.00).toFixed(2)]);
-    const [hayTr5, setHayTr5] = useState(["5% Alfalfa", "Heavy Rain", (0).toFixed(0), "$  "+(35.00).toFixed(2)]);
-    const [hayTr6, setHayTr6] = useState(["30% Alfalfa", "Heavy Rain", (0).toFixed(0), "$  "+(35.00).toFixed(2)]);
+    const [hayTr3, setHayTr3] = useState(["5% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(100.00).toFixed(2)]);
+    const [hayTr4, setHayTr4] = useState(["30% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(95.00).toFixed(2)]);
+    const [hayTr5, setHayTr5] = useState(["5% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)]);
+    const [hayTr6, setHayTr6] = useState(["30% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)]);
+
+    const [honeyHeader, setHoneyHeader] = useState(["Honey Type", "Honey Size", "Quantity", "Price/Unit"]);
+    const [honeyTr1, setHoneyTr1] = useState(["Wildflower/Alfalfa", "340ml(12oz)", (6).toFixed(0), "$ "+(10.00).toFixed(2)]);
+    const [honeyTr2, setHoneyTr2] = useState(["Wildflower/Alfalfa", "500ml(17.6oz)", (0).toFixed(0), "$ "+(15.00).toFixed(2)]);
 
     return (
         <AppContext.Provider
@@ -29,6 +33,12 @@ export const ContextProvider = () => {
             setHayTr5: setHayTr5,
             hayTr6: hayTr6,
             setHayTr6: setHayTr6,
+            honeyHeader: honeyHeader,
+            setHoneyHeader: setHoneyHeader,
+            honeyTr1: honeyTr1,
+            setHoneyTr1: setHoneyTr1,
+            honeyTr2: honeyTr2,
+            setHoneyTr2: setHoneyTr2,
         }}>
             <App />
         </AppContext.Provider>
--- hopfe-farm/src/components/Honey.js
+++ hopfe-farm/src/components/Honey.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
 import '../App.css';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { AppContext } from './AppContext.js';
 
-const Honey= () => {
+const Honey = () => {
+    const appContext = React.useContext(AppContext);
     return (
-        <div className="home-slogan">
-            Providing Albertans with Quality Honey
-        </div>
+        <body className="honey-app">
+            <h1 className="honey-app-header">Honey For Sale</h1>
+            <span className="honey-contact">Please contact Garth Hopfe for honey orders.</span>
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th className="honey-table-header">{appContext.honeyHeader[0]}</Th>
+                        <Th className="honey-table-header">{appContext.honeyHeader[1]}</Th>
+                        <Th className="honey-table-header">{appContext.honeyHeader[2]}</Th>
+                        <Th className="honey-table-header">{appContext.honeyHeader[3]}</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr className="honey-tr">
+                        <Td className="honey-type-td">{appContext.honeyTr1[0]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr1[1]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr1[2]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr1[3]}</Td>
+                    </Tr>
+                    <Tr className="honey-tr">
+                        <Td className="honey-type-td">{appContext.honeyTr2[0]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr2[1]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr2[2]}</Td>
+                        <Td className="honey-td">{appContext.honeyTr2[3]}</Td>
+                    </Tr>
+                </Tbody>
+            </Table>
+        </body>
     )
 }
 
