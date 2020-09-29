0b0d844ebdafd10918f6306670cafa4ba8b43417
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-09-29 15:56:19 -0600

    created app file, set up register, login and user  endpoints

---
+++ hopfe-farm/src/server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from db import db
+
+from resources.users import UserRegister, User, UserLogin
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+app.secret_key = 'development'
+
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWTManager(app)
+
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user')
+api.add_resource(UserLogin, '/login')
+
+if __name__ == '__main__':
+    print("--- Starting", __file__)
+    db.init_app(app)
+    app.run(debug=True, use_reloader=True, port=5000)
