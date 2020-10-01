7ee7c05c69c0ff0efcd3e5f385612346c04e182d
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-10-01 14:04:17 -0600

    removed useEffect array

--- hopfe-farm/src/components/Fetch.js
+++ hopfe-farm/src/components/Fetch.js
@@ -29,7 +29,7 @@ export const useFetchWithAuth = (url = '', urlMethod = 'GET', token = '') => {
             setLoading(false);
         }
         fetchData();
-    }, []);
+    });
 
     return { data, loading };
 };
