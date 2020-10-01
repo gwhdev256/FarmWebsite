604c23ff0c30012bd5ba417f8fd84bbfb4c84ae1
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-10-01 14:03:49 -0600

    added useEffect

--- hopfe-farm/src/components/Contact.js
+++ hopfe-farm/src/components/Contact.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
+import { AppContext } from './AppContext.js';
 
 const Contact = () => {
+    const appContext = React.useContext(AppContext);
+
+    useEffect(() => {
+        appContext.setSelectedIcon("contact");
+    });
+
     return (
         <div className="contact-info">
             Garth Hopfe
