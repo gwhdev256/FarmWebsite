4d94473eaab18b794ac4b9c494fc47ffe366f6b5
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-10-01 14:00:35 -0600

    moved react router tags to index and AppRouter

--- hopfe-farm/src/App.js
+++ hopfe-farm/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
-import Admin from './components/Admin.js';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import AppRouter from './AppRouter.js';
 import { ContextProvider } from './components/AppContext.js';
 
 const App = () => {
   return (
     <ContextProvider>
-      <Router>
-        <div className="Component-display">
-          <Switch>
-            <Route path="/" exact component={AppRouter} />
-            <Route path="/admin" component={Admin} />
-          </Switch>
-        </div>
-      </Router>
+      <AppRouter/>
     </ContextProvider>
   );
 }
