35c548023461ea6328ab0ce05b33a252d6903086
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-01-19 15:40:22 -0700

    simplified hay and honey row states

--- hopfe-farm/src/components/AppContext.js
+++ hopfe-farm/src/components/AppContext.js
@@ -10,27 +10,25 @@ export const ContextProvider = (props) => {
     const [token, setToken] = useState(null);
     
     const [hayHeader, setHayHeader] = useState(["Hay Type", "Bale Quality", "Availability", "Price/Bale"]);
-    const [hayTr1, setHayTr1] = useState(["5% Alfalfa", "No Rain", (5).toFixed(0), "$ "+(125.00).toFixed(2)]);
-    const [hayTr2, setHayTr2] = useState(["30% Alfalfa", "No Rain", (39).toFixed(0), "$ "+(120.00).toFixed(2)]);
-    const [hayTr3, setHayTr3] = useState(["5% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(100.00).toFixed(2)]);
-    const [hayTr4, setHayTr4] = useState(["30% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(95.00).toFixed(2)]);
-    const [hayTr5, setHayTr5] = useState(["5% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)]);
-    const [hayTr6, setHayTr6] = useState(["30% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)]);
+    const [hayTr, setHayTr] = useState(
+                                    [
+                                        ["5% Alfalfa", "No Rain", (5).toFixed(0), "$ "+(125.00).toFixed(2)],
+                                        ["30% Alfalfa", "No Rain", (39).toFixed(0), "$ "+(120.00).toFixed(2)],
+                                        ["5% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(100.00).toFixed(2)],
+                                        ["30% Alfalfa", "Some Rain", (0).toFixed(0), "$ "+(95.00).toFixed(2)],
+                                        ["5% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)],
+                                        ["30% Alfalfa", "Heavy Rain", (0).toFixed(0), "$ "+(20.00).toFixed(2)]
+                                    ]
+    );
 
     const [honeyHeader, setHoneyHeader] = useState(["Honey Type", "Honey Size", "Availability", "Price/Unit"]);
-    const [honeyTr1, setHoneyTr1] = useState(["Wildflower/Alfalfa", "340ml(12oz)", (6).toFixed(0), "$ "+(10.00).toFixed(2)]);
-    const [honeyTr2, setHoneyTr2] = useState(["Wildflower/Alfalfa", "500ml(17.6oz)", (0).toFixed(0), "$ "+(15.00).toFixed(2)]);
-
-    const selectedElement = (event) => {
-        setSelectedIcon(event.target.name);
-    }
-
-    const handleOnChange = (event) => {
-        let currentArray = eval(event.target.alt);
-        let currArrIndex = Number(event.target.className);
-        currentArray[currArrIndex] = event.target.value;
-        eval(event.target.name)(currentArray);
-    }
+    const [honeyTr, setHoneyTr] = useState(
+                                        [
+                                            ["Wildflower/Alfalfa", "340ml(12oz)", (6).toFixed(0), "$ "+(10.00).toFixed(2)],
+                                            ["Wildflower/Alfalfa", "500ml(17.6oz)", (0).toFixed(0), "$ "+(15.00).toFixed(2)]
+                                        ]
+                                        
+    );
 
     
     return (
@@ -38,28 +36,14 @@ export const ContextProvider = (props) => {
         value={{
             hayHeader,
             setHayHeader,
-            hayTr1,
-            setHayTr1,
-            hayTr2,
-            setHayTr2,
-            hayTr3,
-            setHayTr3,
-            hayTr4,
-            setHayTr4,
-            hayTr5,
-            setHayTr5,
-            hayTr6,
-            setHayTr6,
+            hayTr,
+            setHayTr,
             honeyHeader,
             setHoneyHeader,
-            honeyTr1,
-            setHoneyTr1,
-            honeyTr2,
-            setHoneyTr2,
+            honeyTr,
+            setHoneyTr,
             selectedIcon,
             setSelectedIcon,
-            selectedElement,
-            handleOnChange,
             apiUrl,
             setApiUrl,
             homeUrl,
