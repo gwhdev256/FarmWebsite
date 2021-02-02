d2344a0714c28b568ea7db83334042300fff8fc9
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-02-01 18:51:09 -0700

    changed table model to contact_data model

---
+++ hopfe-farm/src/server/models/contact_data.py
@@ -0,0 +1,25 @@
+from db import db
+
+class ContactDataModel(db.Model):
+    __tablename__ = "Contacts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    stored_string = db.Column(db.String(500), unique = True, nullable = False)
+
+    def __init__(self, stored_string):
+        self.stored_string = stored_string
+
+    def json(self):
+        return {'stored_string': self.stored_string}
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
