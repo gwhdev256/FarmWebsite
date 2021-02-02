dba6a3ea81bad3edadbc22a2fcd846de8353ffe1
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-02-01 18:51:28 -0700

    changed tables resource to contact_data resource

---
+++ hopfe-farm/src/server/resources/contact_data.py
@@ -0,0 +1,51 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from models.contact_data import ContactDataModel
+
+
+class CreateContactData(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('stored_string',
+        type=str,
+        required=True,
+        help="Body must contain table_name"
+    )
+    parser.add_argument('_id',
+        type=int
+    )
+
+    @jwt_required
+    def post(self):
+        data = CreateContactData.parser.parse_args()
+
+        if ContactDataModel.find_by_id(data["_id"]):
+            return {"message": "Contact data with that id already exists."}, 404
+
+        contact_data = ContactDataModel(data["stored_string"])
+
+        try:
+            contact_data.save_to_db()
+            return {"message": "Table '{}' created successfully.".format(data['table_name'])}, 201
+        except:
+            return {"message": "Something went wrong."}, 500
+
+class ContactData(Resource):
+    @classmethod
+    def get(cls, _id):
+        contact_data = ContactDataModel.find_by_id(_id)
+
+        if contact_data:
+            return contact_data.json(), 200
+        return {"message": "Contact data not found."}, 400
+
+    @classmethod
+    @jwt_required
+    def delete(cls, _id):
+        contact_data = ContactDataModel.find_by_id(_id)
+
+        if contact_data:
+            contact_data.delete_from_db()
+            return {"message": "Contact data deleted successfully."}, 200
+        return {"message": "Contact data not found."}, 404
+        
