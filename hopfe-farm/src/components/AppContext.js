436abfa0d7774dabf63010c19be3b71c522ccda5
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-02-05 17:45:10 -0700

    exported hayTrHandler function

--- hopfe-farm/src/components/AppContext.js
+++ hopfe-farm/src/components/AppContext.js
@@ -64,7 +64,8 @@ export const ContextProvider = (props) => {
             loggedIn,
             setLoggedIn,
             token,
-            setToken
+            setToken,
+            hayTrHandler
         }}>
             {props.children}
         </AppContext.Provider>
