89ba75fa206e7967d082490ec45f6cd4c65ce657
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-03-11 18:01:50 -0700

    WIP - displaying contact data

--- hopfe-farm/src/components/Contact.js
+++ hopfe-farm/src/components/Contact.js
@@ -13,9 +13,13 @@ const Contact = () => {
         initialLoad();
     }, [appContext]);
 
+    const createContacts = appContext.contactInfo.map(() => {
+        //maybe forEach loop? extract all contact data and display it
+    })
+
     return (
         <div className="contact-info">
-            Garth Hopfe
+            {createContacts}
         </div>
     )
 }
