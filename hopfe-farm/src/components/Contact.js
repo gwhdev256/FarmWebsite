8c1cdff4d91504944c2c6ea79972bb792404e991
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-03-13 16:19:15 -0700

    added loading spinner and state, added keys to contact divs

--- hopfe-farm/src/components/Contact.js
+++ hopfe-farm/src/components/Contact.js
@@ -1,32 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { AppContext } from './AppContext.js';
+import { Ellipsis } from 'react-spinners-css';
 
 const Contact = () => {
     const appContext = React.useContext(AppContext);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const initialLoad = () => {
             appContext.setSelectedIcon("contact");
             appContext.contactInfoLoader();
+            setLoaded(true);
         }
         initialLoad();
-    }, [appContext]);
+        // eslint-disable-next-line
+    }, []);
 
     const createContacts = appContext.contactInfo.map((contact) => {
         return (
-            <div className="contact-container">
-                <div className="contact-name">{contact.ContactName}</div>
-                <div className="contact-title">{contact.JobTitle}</div>
-                <div className="contact-number">{contact.PhoneNumber}</div>
-                <div className="contact-email">{contact.Email}</div>
+            <div key={`${contact.ContactName}`} className="contact-container">
+                <div key={`${contact.ContactName}0`} className="contact-name">{contact.ContactName}</div>
+                <div key={`${contact.ContactName}1`} className="contact-title">{contact.JobTitle}</div>
+                <div key={`${contact.ContactName}2`} className="contact-number">{contact.PhoneNumber}</div>
+                <div key={`${contact.ContactName}3`} className="contact-email">{contact.Email}</div>
             </div>
         )
     })
 
     return (
         <div className="contact-info">
-            {createContacts}
+            {loaded ? createContacts : <div className="spinner"><Ellipsis color="whitesmoke"/></div>}
         </div>
     )
 }
