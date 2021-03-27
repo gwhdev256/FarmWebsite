1e875d7532691b2322ee2a9ed3a52ba32d4f41c1
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-03-26 23:44:47 -0600

    added honey jpg and description

--- hopfe-farm/src/components/Home.js
+++ hopfe-farm/src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../App.css';
 
+import honey from '../images/HoneyPic1.jpg';
+
 const Home = () => {
     return (
-        <div className="home-slogan">
-            Providing Albertans with Quality Hay and Honey
+        <div>
+            <div className="home-slogan">
+                Providing Albertans with Quality Hay and Honey
+            </div>
+            <div className="home-honey-info">
+                <img src={honey} className="honey-pic-1"/>
+                <div className="honey-pic-1-description">This is the best honey.</div>
+            </div>
         </div>
     )
 }
