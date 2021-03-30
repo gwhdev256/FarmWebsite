9f1b73282bea8b1dc3efce72256332ec22dc1bfb
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-03-29 18:13:34 -0600

    added hay and trailer images and descriptions

--- hopfe-farm/src/components/Home.js
+++ hopfe-farm/src/components/Home.js
@@ -2,12 +2,22 @@ import React from 'react';
 import '../App.css';
 
 import honey from '../images/HoneyPic1.jpg';
+import bale from '../images/HayBale1.jpg';
+import trailer2 from '../images/TrailerLoad2.jpg';
 
 const Home = () => {
     return (
         <div>
             <div className="home-slogan">
                 Providing Albertans with Quality Hay and Honey
+            </div>
+            <div className="home-hay-info">
+                <img src={bale} className="hay-pic-1"/>
+                <div className="hay-pic-1-description">All Hay is a Mix of Alfalfa, Brohm, Timothy and Orchard Grass available in 1400 lb Round Bales</div>
+            </div>
+            <div className="home-trailer-info">
+                <div className="trailer-pic-2-description">Delivery and/or skidsteer services are also available.</div>
+                <img src={trailer2} className="trailer-pic-2"/>
             </div>
             <div className="home-honey-info">
                 <img src={honey} className="honey-pic-1"/>
