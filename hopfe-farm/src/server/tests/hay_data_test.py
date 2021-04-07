9bf6d3bfcf04246e27227a0f53ffedb28b18a9aa
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-04-06 18:44:43 -0600

    got post and put tests back to functional

--- hopfe-farm/src/server/tests/hay_data_test.py
+++ hopfe-farm/src/server/tests/hay_data_test.py
@@ -24,18 +24,37 @@ def client():
 
 def test_post(client):
     #http://localhost:5000/createhay
+    client.post('/register', json={"username": "TestUser", "password": "TestPass"})
+
+    rv = client.post('/login', json={"username": "TestUser", "password": "TestPass"})
+    json = rv.get_json()
+    assert("access_token" in json)
+    assert(rv.status_code == 200)
+
+    token = json["access_token"]
+
     rv = client.post('/createhay', json={
                                     "HayType": "test type", 
                                     "BaleQuality": "Some Rain",
                                     "Quantity": 100,
                                     "Price": 200.00
-                                    }
-                    )
+                                    },
+                    headers={"Authorization" : f"Bearer {token}"}
+    )
     json = rv.get_json()
     assert(json["message"] == "Data added successfully.")
                     
 def test_put(client):
     #http://localhost:5000/createhay
+    client.post('/register', json={"username": "TestUser1", "password": "TestPass"})
+
+    rv = client.post('/login', json={"username": "TestUser1", "password": "TestPass"})
+    json = rv.get_json()
+    assert("access_token" in json)
+    assert(rv.status_code == 200)
+
+    token = json["access_token"]
+
     rv = client.put('/createhay', json={
                                     "HayType": "test type",
                                     "BaleQuality": "Some Rain",
@@ -43,6 +62,8 @@ def test_put(client):
                                     "Price": 200.00,
                                     "NewHayType": "new test type",
                                     "NewBaleQuality": "No Rain"
-    })
+                                    },
+                    headers={"Authorization" : f"Bearer {token}"}
+    )
     json = rv.get_json()
     assert(json["message"] == "Hay entry updated successfully.")
