c50f20244fc1b78658ee90d9dcfc5cba5b00d61a
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-04-09 17:41:30 -0600

    added alt property to images

--- hopfe-farm/src/components/Home.js
+++ hopfe-farm/src/components/Home.js
@@ -12,15 +12,15 @@ const Home = () => {
                 Providing Albertans with Quality Hay and Honey
             </div>
             <div className="home-hay-info">
-                <img src={bale} className="hay-pic-1"/>
+                <img src={bale} alt="home-hay" className="hay-pic-1"/>
                 <div className="hay-pic-1-description">All Hay is a Mix of Alfalfa, Brohm, Timothy and Orchard Grass available in 1400 lb Round Bales</div>
             </div>
             <div className="home-trailer-info">
                 <div className="trailer-pic-2-description">Delivery and/or skidsteer services are also available.</div>
-                <img src={trailer2} className="trailer-pic-2"/>
+                <img src={trailer2} alt="home-trailer" className="trailer-pic-2"/>
             </div>
             <div className="home-honey-info">
-                <img src={honey} className="honey-pic-1"/>
+                <img src={honey} alt="home-honey" className="honey-pic-1"/>
                 <div className="honey-pic-1-description">This is the best honey.</div>
             </div>
         </div>
