34cc5d3c3d3abe4b13af3f9032889fa225b09893
nhopfe <55861853+nhopfe@users.noreply.github.com>
2021-04-13 15:50:31 -0600

    WIP - started honey tests

--- hopfe-farm/src/server/tests/honey_data_test.py
+++ hopfe-farm/src/server/tests/honey_data_test.py
@@ -15,9 +15,28 @@ def client():
     json = rv.get_json()
     setup_token = json["access_token"]
     client.delete('/user', headers={"Authorization" : f"Bearer {setup_token}"})
-    client.post('/register', json={"username": "TestUser1", "password": "TestPass"})
-    rv = client.post('/login', json={"username": "TestUser1", "password": "TestPass"})
-    json = rv.get_json()
-    setup_token2 = json["access_token"]
-    client.delete('/user', headers={"Authorization" : f"Bearer {setup_token2}"})
     yield client
+    client.post('/register', json={"username": "TestUser", "password": "TestPass"})
+    rv = client.post('/login', json={"username": "TestUser", "password": "TestPass"})
+    json = rv.get_json()
+    setup_token = json["access_token"]
+    client.delete('/user', headers={"Authorization" : f"Bearer {setup_token}"})
+
+def test_post(client):
+    #http://localhost:5000/createhoney
+    client.post('/register', json={"username": "TestUser", "password": "TestPass"})
+    rv = client.post('/login', json={"username": "TestUser", "password": "TestPass"})
+    json = rv.get_json()
+    token = json["access_token"]
+
+    rv = client.post('/createhoney', json={
+                                        "HoneyType": "TestType",
+                                        "HoneySize": 1,
+                                        "Quantity": 1,
+                                        "Price": 1000.00
+                                    },
+                                    headers={"Authorization" : f"Bearer {token}"}
+                    )
+    json = rv.get_json()
+    assert(rv.status_code == 201)
+    assert(json["message"] == "Data added successfully.")
